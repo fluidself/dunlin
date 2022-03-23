2940a74f09007e604ef024c733573cf80f2700da
fluidself <me@fluidself.org>
2022-03-23 19:58:32 +0100

    Refactors app sidebar UI

--- components/CreateJoinRenameDeckModal.tsx
+++ components/CreateJoinRenameDeckModal.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore
 import LitJsSdk from 'lit-js-sdk';
 import { useMemo, useState } from 'react';
-import { IconFolderPlus, IconGitPullRequest } from '@tabler/icons';
+import { IconFolderPlus, IconGitPullRequest, IconPencil } from '@tabler/icons';
 import { toast } from 'react-toastify';
 import useSWR from 'swr';
 import supabase from 'lib/supabase';
@@ -12,16 +12,18 @@ import { useAuth } from 'utils/useAuth';
 import { AuthSig } from 'types/lit';
 import useHotkeys from 'utils/useHotkeys';
 import Button from 'components/home/Button';
+import { useCurrentDeck } from 'utils/useCurrentDeck';
 
 type Props = {
-  type: 'create' | 'join';
+  type: 'create' | 'join' | 'rename';
   closeModal: () => void;
 };
 
-export default function CreateOrJoinDeckModal(props: Props) {
+export default function CreateJoinRenameDeckModal(props: Props) {
   const { type, closeModal } = props;
 
   const { user } = useAuth();
+  const { deck } = useCurrentDeck();
   const { data: decks } = useSWR(user ? 'decks' : null, () => selectDecks(user?.id), { revalidateOnFocus: false });
   const [inputText, setInputText] = useState<string>('');
   const [processing, setProcessing] = useState<boolean>(false);
@@ -48,6 +50,22 @@ export default function CreateOrJoinDeckModal(props: Props) {
     }
 
     toast.success(`Successfully created ${deck.deck_name}`);
+    setProcessing(false);
+    closeModal();
+    window.location.assign(`${process.env.BASE_URL}/app/${deck.id}`);
+  };
+
+  const renameDeck = async () => {
+    if (!user || !deck || !inputText) return;
+
+    const { data, error } = await supabase.from<Deck>('decks').update({ deck_name: inputText }).eq('id', deck.id).single();
+
+    if (error || !data) {
+      toast.error('There was an error updating the DECK');
+      return;
+    }
+
+    toast.success(`Successfully renamed ${data.deck_name}`);
     setProcessing(false);
     closeModal();
     window.location.assign(`${process.env.BASE_URL}/app/${deck.id}`);
@@ -106,13 +124,14 @@ export default function CreateOrJoinDeckModal(props: Props) {
     }
   };
 
-  const title = type === 'create' ? 'Create a new DECK' : 'Join a DECK';
-  const icon =
-    type === 'create' ? (
-      <IconFolderPlus className="ml-4 mr-1 text-gray-200" size={32} />
-    ) : (
-      <IconGitPullRequest className="ml-4 mr-1 text-gray-200" size={32} />
-    );
+  const headings = { create: 'Create a new DECK', join: 'Join a DECK', rename: 'Rename this DECK' };
+  const icons = {
+    create: <IconFolderPlus className="ml-4 mr-1 text-gray-200" size={32} />,
+    join: <IconGitPullRequest className="ml-4 mr-1 text-gray-200" size={32} />,
+    rename: <IconPencil className="ml-4 mr-1 text-gray-200" size={32} />,
+  };
+  const placeholders = { create: 'Enter DECK name', join: 'Enter DECK ID', rename: 'Enter new DECK name' };
+  const onClickHandlers = { create: createNewDeck, join: verifyAccess, rename: renameDeck };
 
   return (
     <div className="fixed inset-0 z-20 overflow-y-auto">
@@ -120,28 +139,28 @@ export default function CreateOrJoinDeckModal(props: Props) {
       <div className="flex justify-center px-6 max-h-screen-80 my-screen-10">
         <div className="flex flex-col z-30 w-full max-w-screen-sm rounded shadow-popover bg-gray-800 text-gray-200">
           <div className="flex items-center flex-shrink-0 w-full">
-            {icon}
-            <span className="text-xl py-4 px-2 border-none rounded-tl rounded-tr focus:ring-0 bg-gray-800">{title}</span>
+            {icons[type]}
+            <span className="text-xl py-4 px-2 border-none rounded-tl rounded-tr focus:ring-0 bg-gray-800">{headings[type]}</span>
           </div>
           <div className="px-4 py-4 flex-1 w-full overflow-y-auto border-t rounded-bl rounded-br bg-gray-700 border-gray-700">
             <input
               type="text"
               className="w-full py-4 px-2 text-xl border-none rounded-tl rounded-tr focus:ring-0 bg-gray-800 text-gray-200"
-              placeholder={type === 'create' ? 'Enter DECK name' : 'Enter DECK ID'}
+              placeholder={placeholders[type]}
               value={inputText}
               onChange={e => setInputText(e.target.value)}
               autoFocus
               autoComplete="off"
-              maxLength={type === 'create' ? 20 : 40}
+              maxLength={type === 'create' || type === 'rename' ? 20 : 40}
             />
             <Button
               className={`my-4 ${processing ? 'bg-gray-800 text-gray-400 hover:bg-gray-800 hover:text-gray-400' : ''}`}
               primary
-              onClick={() => (type === 'create' ? createNewDeck() : verifyAccess())}
+              onClick={onClickHandlers[type]}
               disabled={processing}
               loading={processing}
             >
-              {type === 'create' ? 'Create' : 'Join'}
+              {type}
             </Button>
           </div>
         </div>
--- components/sidebar/SidebarHeader.tsx
+++ components/sidebar/SidebarHeader.tsx
@@ -1,43 +1,78 @@
-import { IconChevronsLeft, IconCopy } from '@tabler/icons';
+import { Menu } from '@headlessui/react';
+import {
+  IconLogout,
+  IconSelector,
+  IconChevronsLeft,
+  IconBrandGithub,
+  IconShare,
+  IconGitPullRequest,
+  IconFolderPlus,
+  IconPencil,
+} from '@tabler/icons';
+import { useAuth } from 'utils/useAuth';
 import { useStore } from 'lib/store';
-import copyToClipboard from 'utils/copyToClipboard';
-import { useCurrentDeck } from 'utils/useCurrentDeck';
-import { addEllipsis } from 'utils/string';
 import Tooltip from 'components/Tooltip';
+import { DropdownItem } from 'components/Dropdown';
 
-export default function Header() {
-  const { deck } = useCurrentDeck();
+type Props = {
+  setIsShareModalOpen: (arg0: boolean) => void;
+  setCreateJoinRenameModal: (arg0: { open: boolean; type: string }) => void;
+};
+
+export default function Header(props: Props) {
+  const { setIsShareModalOpen, setCreateJoinRenameModal } = props;
+  const { signOut } = useAuth();
   const setIsSidebarOpen = useStore(state => state.setIsSidebarOpen);
 
   return (
-    <div className="relative">
-      <div className="flex items-center justify-between w-full py-2 pl-6 overflow-x-hidden text-left text-gray-800  dark:text-gray-200  overflow-ellipsis whitespace-nowrap focus:outline-none cursor-default border-b border-gray-700">
-        <div className="flex flex-col flex-1">
-          <span className="mr-1 font-semibold select-none hero-decoration">DECK</span>
-          {deck && (
-            <div className="flex flex-row items-center">
-              <span className="text-xs">{addEllipsis(deck.id)}</span>
-              <button
-                className="flex items-center text-gray-200 hover:bg-gray-600 hover:text-gray-200 ml-2 rounded p-1"
-                onClick={async () => await copyToClipboard(deck.id)}
-              >
-                <IconCopy size={18} className="" />
-              </button>
+    <>
+      <div className="relative">
+        <Menu>
+          <Menu.Button className="flex items-center justify-between w-full py-2 pl-6 overflow-x-hidden text-left text-gray-800 hover:bg-gray-200 active:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 overflow-ellipsis whitespace-nowrap focus:outline-none border-b border-gray-700">
+            <div className="flex items-center flex-1">
+              <span className="mr-1 font-semibold select-none">DECK</span>
+              <IconSelector size={18} className="text-gray-500 dark:text-gray-400" />
             </div>
-          )}
-        </div>
-        <Tooltip content="Collapse sidebar (Ctrl+\)" placement="right">
-          <span
-            className="p-1 mr-2 rounded hover:bg-gray-300 active:bg-gray-400 dark:hover:bg-gray-600 dark:active:bg-gray-500 cursor-pointer"
-            onClick={e => {
-              e.stopPropagation();
-              setIsSidebarOpen(false);
-            }}
-          >
-            <IconChevronsLeft className="text-gray-500 dark:text-gray-400" />
-          </span>
-        </Tooltip>
+            <Tooltip content="Collapse sidebar (Ctrl+\)" placement="right">
+              <span
+                className="p-1 mr-2 rounded hover:bg-gray-300 active:bg-gray-400 dark:hover:bg-gray-600 dark:active:bg-gray-500"
+                onClick={e => {
+                  e.stopPropagation();
+                  setIsSidebarOpen(false);
+                }}
+              >
+                <IconChevronsLeft className="text-gray-500 dark:text-gray-400" />
+              </span>
+            </Tooltip>
+          </Menu.Button>
+          <Menu.Items className="absolute z-20 w-56 overflow-hidden bg-white rounded left-6 top-full shadow-popover dark:bg-gray-800 focus:outline-none border border-gray-500">
+            <DropdownItem className="" onClick={() => setIsShareModalOpen(true)}>
+              <IconShare size={18} className="mr-1" />
+              <span>Share</span>
+            </DropdownItem>
+            <DropdownItem className="" onClick={() => setCreateJoinRenameModal({ open: true, type: 'rename' })}>
+              <IconPencil size={18} className="mr-1" />
+              <span>Rename</span>
+            </DropdownItem>
+            <DropdownItem className="" onClick={() => setCreateJoinRenameModal({ open: true, type: 'join' })}>
+              <IconGitPullRequest size={18} className="mr-1" />
+              <span>Join</span>
+            </DropdownItem>
+            <DropdownItem className="" onClick={() => setCreateJoinRenameModal({ open: true, type: 'create' })}>
+              <IconFolderPlus size={18} className="mr-1" />
+              <span>Create</span>
+            </DropdownItem>
+            <DropdownItem as="a" className="border-t dark:border-gray-700" href="https://github.com/fluidself/deck">
+              <IconBrandGithub size={18} className="mr-1" />
+              <span>Source Code</span>
+            </DropdownItem>
+            <DropdownItem className="" onClick={signOut}>
+              <IconLogout size={18} className="mr-1" />
+              <span>Sign Out</span>
+            </DropdownItem>
+          </Menu.Items>
+        </Menu>
       </div>
-    </div>
+    </>
   );
 }
