1d1950dd8b5b0e42e1bdc595f4157bfb13093b10
fluidself <me@fluidself.org>
2022-03-23 21:24:35 +0100

    Changes signout route to POST

--- pages/api/signout.ts
+++ pages/api/signout.ts
@@ -5,8 +5,8 @@ import { ironOptions } from 'constants/iron-session';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
-  if (method !== 'GET') {
-    res.setHeader('Allow', ['GET']);
+  if (method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${method} Not Allowed`);
   }
 
