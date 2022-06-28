32b4a5bb6c629803da2c473fcedd4cf8c49f6d51
fluidself <me@fluidself.org>
2022-06-28 12:29:36 +0200

    Improves handling of note permission update

--- components/settings/EditorSettings.tsx
+++ components/settings/EditorSettings.tsx
@@ -8,21 +8,40 @@ import Toggle from 'components/Toggle';
 import Button from 'components/home/Button';
 
 export default function EditorSettings() {
-  const { id: deckId, author_only_notes, author_control_notes } = useCurrentDeck();
+  const { id: deckId } = useCurrentDeck();
   const setAuthorOnlyNotes = useStore(state => state.setAuthorOnlyNotes);
+  const updateNote = useStore(state => state.updateNote);
 
-  const [authorOnly, setAuthorOnly] = useState(author_only_notes);
-  const [authorControl, setAuthorControl] = useState(author_control_notes);
+  const [dbSettings, setDbSettings] = useState<any>({});
+  const [authorOnly, setAuthorOnly] = useState<boolean>();
+  const [authorControl, setAuthorControl] = useState<boolean>();
   const [hasChanges, setHasChanges] = useState(false);
   const [processing, setProcessing] = useState(false);
 
+  const initSettings = async () => {
+    const { data: deckSettings } = await supabase
+      .from<Deck>('decks')
+      .select('author_only_notes, author_control_notes')
+      .eq('id', deckId)
+      .single();
+    if (!deckSettings) return;
+    setDbSettings({ authorOnly: deckSettings.author_only_notes, authorControl: deckSettings.author_control_notes });
+    setAuthorOnly(deckSettings.author_only_notes);
+    setAuthorControl(deckSettings.author_control_notes);
+  };
+
   useEffect(() => {
-    if (authorOnly !== author_only_notes || authorControl !== author_control_notes) {
+    initSettings();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (authorOnly !== dbSettings.authorOnly || authorControl !== dbSettings.authorControl) {
       setHasChanges(true);
     } else {
       setHasChanges(false);
     }
-  }, [author_only_notes, author_control_notes, authorOnly, authorControl]);
+  }, [dbSettings, authorOnly, authorControl]);
 
   useEffect(() => {
     if (authorOnly === false) {
@@ -50,6 +69,10 @@ export default function EditorSettings() {
         }
       }
 
+      for (const note of noteUpdates) {
+        updateNote({ id: note.id, author_only: note.author_only });
+      }
+
       await supabase.from<Note>('notes').upsert(noteUpdates);
       await supabase
         .from<Deck>('decks')
@@ -57,10 +80,10 @@ export default function EditorSettings() {
         .eq('id', deckId)
         .single();
 
-      setAuthorOnlyNotes(authorOnly);
+      if (typeof authorOnly !== 'undefined') setAuthorOnlyNotes(authorOnly);
       setHasChanges(false);
+      await initSettings();
       toast.success('Settings updated!');
-      window.location.assign(`${process.env.BASE_URL}/app/${deckId}`);
     } catch (error) {
       toast.error('There was an error updating the settings');
       setProcessing(false);
@@ -69,41 +92,51 @@ export default function EditorSettings() {
     setProcessing(false);
   };
 
+  const renderSettings = () => {
+    if (typeof authorOnly === 'undefined' || typeof authorControl === 'undefined') return null;
+
+    return (
+      <>
+        <div>
+          <div className="mb-4">
+            <h2 className="text-lg font-medium">View-only Notes</h2>
+            <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
+              If on, notes can only be edited by their original author and the DECK owner.
+            </p>
+          </div>
+          <div className="flex items-center">
+            <span className="text-sm text-gray-600 dark:text-gray-300">Off</span>
+            <Toggle className="mx-2" id="2" isChecked={authorOnly} setIsChecked={setAuthorOnly} />
+            <span className="text-sm text-gray-600 dark:text-gray-300">On</span>
+          </div>
+          {authorOnly && (
+            <>
+              <div className="my-4">
+                <h4 className="text-sm font-medium">Allow Note Author Control</h4>
+                <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
+                  If on, DECK members can toggle view-only status for their own notes.
+                </p>
+              </div>
+              <div className="flex items-center">
+                <span className="text-sm text-gray-600 dark:text-gray-300">Off</span>
+                <Toggle className="mx-2" id="3" isChecked={authorControl} setIsChecked={setAuthorControl} />
+                <span className="text-sm text-gray-600 dark:text-gray-300">On</span>
+              </div>
+            </>
+          )}
+        </div>
+        <div className={`flex justify-end ${!hasChanges && 'hidden'}`}>
+          <Button primary disabled={!hasChanges || processing} loading={processing} onClick={onSaveChanges}>
+            Save Changes
+          </Button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="flex-1 w-full h-full p-6 flex flex-col justify-between overflow-y-auto dark:bg-gray-800 dark:text-gray-100">
-      <div>
-        <div className="mb-4">
-          <h2 className="text-lg font-medium">View-only Notes</h2>
-          <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
-            If on, notes can only be edited by their original author and the DECK owner.
-          </p>
-        </div>
-        <div className="flex items-center">
-          <span className="text-sm text-gray-600 dark:text-gray-300">Off</span>
-          <Toggle className="mx-2" id="2" isChecked={authorOnly} setIsChecked={setAuthorOnly} />
-          <span className="text-sm text-gray-600 dark:text-gray-300">On</span>
-        </div>
-        {authorOnly && (
-          <>
-            <div className="my-4">
-              <h4 className="text-sm font-medium">Allow Note Author Control</h4>
-              <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
-                If on, DECK members can toggle view-only status for their own notes.
-              </p>
-            </div>
-            <div className="flex items-center">
-              <span className="text-sm text-gray-600 dark:text-gray-300">Off</span>
-              <Toggle className="mx-2" id="3" isChecked={authorControl} setIsChecked={setAuthorControl} />
-              <span className="text-sm text-gray-600 dark:text-gray-300">On</span>
-            </div>
-          </>
-        )}
-      </div>
-      <div className={`flex justify-end ${!hasChanges && 'hidden'}`}>
-        <Button primary disabled={!hasChanges || processing} loading={processing} onClick={onSaveChanges}>
-          Save Changes
-        </Button>
-      </div>
+      {renderSettings()}
     </div>
   );
 }
