745de32e8564714e203cf199bf21cadbb6194e34
fluidself <me@fluidself.org>
2022-11-01 13:50:05 +0100

    Refactors upsertNote and optimistically updates store first

--- lib/api/upsertNote.ts
+++ lib/api/upsertNote.ts
@@ -1,26 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
 import { store } from 'lib/store';
 import supabase from 'lib/supabase';
 import type { Note, Deck } from 'types/supabase';
+import type { DecryptedNote } from 'types/decrypted';
 import type { PickPartial } from 'types/utils';
-import { decryptNote } from 'utils/encryption';
+import { encryptNote } from 'utils/encryption';
 
-export type NoteUpsert = PickPartial<Note, 'id' | 'content' | 'created_at' | 'updated_at'>;
+export type NoteUpsert = PickPartial<DecryptedNote, 'id' | 'created_at' | 'updated_at'>;
 
-export default async function upsertNote(note: NoteUpsert, key: string) {
-  const { data, error } = await supabase
-    .from<Note>('notes')
-    .upsert({ ...note, updated_at: new Date().toISOString() })
-    .single();
+export default async function upsertNote(noteUpsert: NoteUpsert, key: string) {
+  const note = {
+    ...noteUpsert,
+    id: noteUpsert.id ?? uuidv4(),
+    created_at: noteUpsert.created_at ?? new Date().toISOString(),
+    updated_at: new Date().toISOString(),
+  };
 
-  // Refreshes the list of notes in the sidebar
-  if (data) {
-    const decryptedNote = decryptNote(data, key);
-    store.getState().upsertNote(decryptedNote);
+  store.getState().upsertNote(note);
 
-    await supabase.from<Deck>('decks').update({ note_tree: store.getState().noteTree }).eq('id', note.deck_id);
-  } else if (error) {
-    console.error(error);
-  }
+  const encryptedNote = encryptNote(note, key);
+  await supabase.from<Note>('notes').upsert(encryptedNote).single();
+  await supabase.from<Deck>('decks').update({ note_tree: store.getState().noteTree }).eq('id', note.deck_id);
 
-  return data;
+  return note;
 }
