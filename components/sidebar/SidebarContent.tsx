7cd82becba35a05351c5dc60b5cbb16078ab40f7
fluidself <me@fluidself.org>
2022-11-22 16:31:03 +0100

    Adds top margin to sidebar content

--- components/sidebar/SidebarContent.tsx
+++ components/sidebar/SidebarContent.tsx
@@ -19,7 +19,7 @@ export default function SidebarContent(props: Props) {
   return (
     <div className={`flex flex-col ${className}`}>
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className="flex flex-col flex-1 overflow-x-hidden">
+      <div className="flex flex-col flex-1 overflow-x-hidden mt-1">
         {activeTab === SidebarTabType.Notes ? <SidebarNotes setIsFindOrCreateModalOpen={setIsFindOrCreateModalOpen} /> : null}
         {activeTab === SidebarTabType.Search ? <SidebarSearch /> : null}
       </div>
