b7ca4bea862bf5d80af47bca03ebd1db5dec1ba5
fluidself <me@fluidself.org>
2022-12-04 14:37:35 +0100

    Only shows note header share button if user is workspace owner

--- components/editor/NoteHeader.tsx
+++ components/editor/NoteHeader.tsx
@@ -181,16 +181,18 @@ export default function NoteHeader() {
                   }}
                 />
               </div>
-              <Tooltip content="Share DECK">
-                <button className={buttonClassName} onClick={() => setShareModalOpen(true)}>
-                  <span className="flex items-center justify-center w-8 h-8">
-                    <IconShare className={iconClassName} />
-                  </span>
-                </button>
-              </Tooltip>
+              {user.id === deckOwner && (
+                <Tooltip content="Share DECK">
+                  <button className={buttonClassName} onClick={() => setShareModalOpen(true)}>
+                    <span className="flex items-center justify-center w-8 h-8">
+                      <IconShare className={iconClassName} />
+                    </span>
+                  </button>
+                </Tooltip>
+              )}
               <NoteHeaderDivider />
               <div className="px-2 pt-1 pb-1 text-sm text-gray-600 overflow-ellipsis dark:text-gray-400">
-                {user ? addEllipsis(user?.id) : ''}
+                {addEllipsis(user.id)}
               </div>
               <Identicon diameter={16} className="w-5 h-5 mr-2" />
               <NoteHeaderDivider />
