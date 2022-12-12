cb7d4912e518a76cbc30634c2235fe716824bdf6
fluidself <me@fluidself.org>
2022-12-12 08:52:07 +0100

    Improves insert table command UX

--- editor/plugins/withTables/commands/insertTable.ts
+++ editor/plugins/withTables/commands/insertTable.ts
@@ -1,20 +1,33 @@
-import { type Location, Transforms } from 'slate';
+import { type Location, Transforms, Editor } from 'slate';
 import { ReactEditor } from 'slate-react';
-import { createTable } from '../nodes';
+import { ElementType } from 'types/slate';
 import type { TablesEditor } from '../TablesEditor';
+import { createTable } from '../nodes';
+import { isInTable } from '../queries';
 
 export function insertTable(
   editor: TablesEditor,
   location: Location | undefined = editor.selection ?? undefined,
   props?: Parameters<typeof createTable>[1],
 ) {
-  if (!location) {
+  if (!location || isInTable(editor)) {
     return false;
   }
 
   Transforms.insertNodes(editor, createTable(editor, props), {
     at: location,
   });
+
+  const createdTable = Editor.next(editor, {
+    match: n => n.type === ElementType.Table,
+  });
+
+  if (createdTable) {
+    Transforms.setSelection(editor, {
+      anchor: { path: [...createdTable[1], 0, 0, 0], offset: 0 },
+      focus: { path: [...createdTable[1], 0, 0, 0], offset: 0 },
+    });
+  }
 
   ReactEditor.focus(editor);
 
