19f8c9874a6b3f38b8b85d361b7969ae2fb6b014
fluidself <me@fluidself.org>
2022-12-22 06:55:01 +0100

    Saves note when changing code block language or toggling checklist item

--- components/editor/elements/CheckListItemElement.tsx
+++ components/editor/elements/CheckListItemElement.tsx
@@ -1,12 +1,12 @@
 import { ChangeEvent, ReactNode, useCallback } from 'react';
-import { Transforms } from 'slate';
-import {
-  ReactEditor,
-  RenderElementProps,
-  useReadOnly,
-  useSlateStatic,
-} from 'slate-react';
+import { createEditor, Transforms } from 'slate';
+import { ReactEditor, RenderElementProps, useReadOnly, useSlateStatic } from 'slate-react';
 import { CheckListItem } from 'types/slate';
+import { useCurrentNote } from 'utils/useCurrentNote';
+import { useCurrentDeck } from 'utils/useCurrentDeck';
+import { encrypt } from 'utils/encryption';
+import updateNote from 'lib/api/updateNote';
+import { store } from 'lib/store';
 
 type Props = {
   element: CheckListItem;
@@ -19,35 +19,39 @@ export default function CheckListItemElement(props: Props) {
   const { attributes, children, element, className } = props;
   const editor = useSlateStatic();
   const readOnly = useReadOnly();
+  const { id: noteId } = useCurrentNote();
+  const { key } = useCurrentDeck();
   const { checked } = element;
 
   const onInputChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      if (readOnly) {
-        return;
-      }
+    async (event: ChangeEvent<HTMLInputElement>) => {
+      if (readOnly) return;
+
       try {
         const path = ReactEditor.findPath(editor, element);
-        const newProperties: Partial<CheckListItem> = {
-          checked: event.target.checked,
-        };
+        const newProperties: Partial<CheckListItem> = { checked: event.target.checked };
         Transforms.setNodes(editor, newProperties, { at: path });
+
+        // Update note locally and in database
+        const noteEditor = createEditor();
+        noteEditor.children = store.getState().notes[noteId].content;
+        Transforms.setNodes(noteEditor, newProperties, { at: path });
+
+        store.getState().updateNote({ id: noteId, content: noteEditor.children });
+
+        const encryptedContent = encrypt(noteEditor.children, key);
+        await updateNote({ id: noteId, content: encryptedContent });
       } catch (e) {
         const message = e instanceof Error ? e.message : e;
-        console.error(
-          `There was an error updating the checklist item: ${message}`
-        );
+        console.error(`There was an error updating the checklist item: ${message}`);
       }
     },
-    [editor, element, readOnly]
+    [editor, element, readOnly, key, noteId],
   );
 
   return (
     <div className={`flex items-center ${className}`} {...attributes}>
-      <div
-        className="flex items-center justify-center mr-2 select-none"
-        contentEditable={false}
-      >
+      <div className="flex items-center justify-center mr-2 select-none" contentEditable={false}>
         <input
           type="checkbox"
           checked={checked}
@@ -58,9 +62,7 @@ export default function CheckListItemElement(props: Props) {
       <span
         contentEditable={!readOnly}
         suppressContentEditableWarning
-        className={`flex-1 ${
-          checked ? 'opacity-60 line-through' : 'opacity-100'
-        }`}
+        className={`flex-1 ${checked ? 'opacity-60 line-through' : 'opacity-100'}`}
       >
         {children}
       </span>
--- components/editor/elements/CodeBlockElement.tsx
+++ components/editor/elements/CodeBlockElement.tsx
@@ -1,8 +1,13 @@
-import { ChangeEvent, ReactNode, useState, useCallback } from 'react';
-import { Transforms } from 'slate';
+import { ChangeEvent, ReactNode, useCallback } from 'react';
+import { createEditor, Transforms } from 'slate';
 import { ReactEditor, RenderElementProps, useSlateStatic, useReadOnly } from 'slate-react';
 import { CODE_BLOCK_LANGUAGES } from 'editor/decorateCodeBlocks';
 import { CodeBlock } from 'types/slate';
+import { useCurrentNote } from 'utils/useCurrentNote';
+import { useCurrentDeck } from 'utils/useCurrentDeck';
+import { encrypt } from 'utils/encryption';
+import updateNote from 'lib/api/updateNote';
+import { store } from 'lib/store';
 
 type Props = {
   element: CodeBlock;
@@ -13,30 +18,38 @@ type Props = {
 
 export default function CodeBlockElement(props: Props) {
   const { attributes, children, element, className } = props;
-
   const editor = useSlateStatic();
   const readOnly = useReadOnly();
+  const { id: noteId } = useCurrentNote();
+  const { key } = useCurrentDeck();
+
   const { lang } = element;
-  const [language, setLanguage] = useState(lang);
   const codeClassName = lang ? `language-${lang}` : '';
 
   const onSelectChange = useCallback(
-    (event: ChangeEvent<HTMLSelectElement>) => {
+    async (event: ChangeEvent<HTMLSelectElement>) => {
       if (readOnly) return;
 
       try {
         const path = ReactEditor.findPath(editor, element);
-        const newProperties: Partial<CodeBlock> = {
-          lang: event.target.value,
-        };
+        const newProperties: Partial<CodeBlock> = { lang: event.target.value };
         Transforms.setNodes(editor, newProperties, { at: path });
-        setLanguage(event.target.value);
+
+        // Update note locally and in database
+        const noteEditor = createEditor();
+        noteEditor.children = store.getState().notes[noteId].content;
+        Transforms.setNodes(noteEditor, newProperties, { at: path });
+
+        store.getState().updateNote({ id: noteId, content: noteEditor.children });
+
+        const encryptedContent = encrypt(noteEditor.children, key);
+        await updateNote({ id: noteId, content: encryptedContent });
       } catch (e) {
         const message = e instanceof Error ? e.message : e;
         console.error(`There was an error updating the language: ${message}`);
       }
     },
-    [editor, element, readOnly],
+    [editor, element, readOnly, key, noteId],
   );
 
   return (
@@ -46,7 +59,7 @@ export default function CodeBlockElement(props: Props) {
     >
       {!readOnly && (
         <select
-          value={language}
+          value={lang ?? ''}
           onChange={onSelectChange}
           contentEditable={false}
           className="bg-transparent absolute top-0.5 right-[-4px] p-0.5 pr-7 border-none focus:ring-0 focus:shadow-none ring-offset-0 text-right cursor-pointer text-[13px]"
