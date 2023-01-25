42341882127c40ccbf76fac82749c49018bc05cd
fluidself <me@fluidself.org>
2023-01-25 07:33:50 +0100

    Restyles sidebar note links to make hierarchy clearer

--- components/sidebar/SidebarHeader.tsx
+++ components/sidebar/SidebarHeader.tsx
@@ -36,20 +36,20 @@ export default function Header(props: Props) {
     <>
       <div className="relative">
         <Menu>
-          <Menu.Button className="flex items-center justify-between w-full py-2 pl-6 overflow-x-hidden text-left text-gray-800 hover:bg-gray-200 active:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 overflow-ellipsis whitespace-nowrap focus:outline-none border-b border-gray-700">
+          <Menu.Button className="flex items-center justify-between w-full py-2 pl-4 overflow-x-hidden text-left text-gray-800 hover:bg-gray-200 active:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 overflow-ellipsis whitespace-nowrap focus:outline-none border-b border-gray-700">
             <div className="flex items-center flex-1">
               <span className="mr-1 font-semibold select-none">Dunlin</span>
               <IconSelector size={18} className="text-gray-500 dark:text-gray-400" />
             </div>
             <Tooltip content={`Collapse sidebar (${modifierKey()}+\\)`} placement="right">
               <span
-                className="p-1 mr-2 rounded hover:bg-gray-300 active:bg-gray-400 dark:hover:bg-gray-600 dark:active:bg-gray-500"
+                className="p-1 mr-1 rounded hover:bg-gray-300 active:bg-gray-400 dark:hover:bg-gray-600 dark:active:bg-gray-500"
                 onClick={e => {
                   e.stopPropagation();
                   setIsSidebarOpen(false);
                 }}
               >
-                <IconChevronsLeft className="text-gray-500 dark:text-gray-400" />
+                <IconChevronsLeft size={20} className="text-gray-500 dark:text-gray-400" />
               </span>
             </Tooltip>
           </Menu.Button>
