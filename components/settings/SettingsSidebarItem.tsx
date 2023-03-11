bbffe60a6fc619a4a41c6c3732de1cd011d73dc7
fluidself <me@fluidself.org>
2023-03-11 09:00:14 +0100

    Fixes settings sidebar item highlighted style

--- components/settings/SettingsSidebarItem.tsx
+++ components/settings/SettingsSidebarItem.tsx
@@ -9,7 +9,7 @@ function SettingsSidebarItem(props: SidebarItemProps, forwardedRef: ForwardedRef
   const { children, className = '', isHighlighted, ...otherProps } = props;
   const itemClassName = classNames(
     'w-full overflow-x-hidden overflow-ellipsis whitespace-nowrap text-gray-300 bg-gray-900',
-    { 'bg-gray-700': isHighlighted },
+    { '!bg-gray-700': isHighlighted },
     className,
   );
   return (
