67f566a50f3ec52f9fedd0252705de0f7cebb5f2
fluidself <me@fluidself.org>
2023-03-20 07:50:14 +0100

    Widens and adds border to command menu

--- components/command-menu/CommandMenuDaemon.tsx
+++ components/command-menu/CommandMenuDaemon.tsx
@@ -57,7 +57,7 @@ export default function CommandMenuDaemon(props: Props) {
   }, [inputText, summoning]);
 
   return (
-    <div className="flex flex-col z-30 w-full max-w-screen-sm bg-white rounded shadow-popover dark:bg-gray-800">
+    <div className="flex flex-col z-30 w-full max-w-2xl bg-white rounded shadow-popover dark:bg-gray-800 border border-gray-700">
       <div className="flex items-center flex-shrink-0 w-full">
         <div className="w-9 h-full relative">
           <IconSearch className="absolute top-4 left-4 text-gray-500" size={20} />
