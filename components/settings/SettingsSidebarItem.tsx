7cc8b9d65404e847b854a3a5acbe4d713540acdb
fluidself <me@fluidself.org>
2023-03-22 11:36:54 +0100

    Adds settings modal light styles

--- components/settings/SettingsSidebarItem.tsx
+++ components/settings/SettingsSidebarItem.tsx
@@ -8,8 +8,11 @@ interface SidebarItemProps extends HTMLAttributes<HTMLDivElement> {
 function SettingsSidebarItem(props: SidebarItemProps, forwardedRef: ForwardedRef<HTMLDivElement>) {
   const { children, className = '', isHighlighted, ...otherProps } = props;
   const itemClassName = classNames(
-    'w-full overflow-x-hidden overflow-ellipsis whitespace-nowrap text-gray-300 bg-gray-900',
-    { '!bg-gray-700': isHighlighted },
+    {
+      'w-full overflow-x-hidden overflow-ellipsis whitespace-nowrap text-gray-800 dark:text-gray-300 dark:bg-gray-900':
+        !isHighlighted,
+    },
+    { 'bg-gray-200 dark:text-gray-300 dark:bg-gray-700': isHighlighted },
     className,
   );
   return (
