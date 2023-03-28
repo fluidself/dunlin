45582d395c220d612c1d72d9fd7d925f2878f271
fluidself <me@fluidself.org>
2023-03-28 07:45:35 +0200

    Renders full-screen loader on dark background

--- components/PageLoading.tsx
+++ components/PageLoading.tsx
@@ -2,7 +2,7 @@ import Logo from './Logo';
 
 export default function PageLoading() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen space-y-4 dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center w-screen h-screen space-y-4 bg-gray-900">
       <Logo width={256} height={256} className="animate-pulse" />
     </div>
   );
