b8bffe9919a470908991a2dfa59ad955ef7ec267
fluidself <me@fluidself.org>
2023-03-30 08:01:02 +0200

    Sets up connect wallet modal

---
+++ components/onboarding/ConnectModal.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { useConnect } from 'wagmi';
+import { IconWallet, IconX } from '@tabler/icons';
+import useHotkeys from 'utils/useHotkeys';
+import { useAuth } from 'utils/useAuth';
+import selectDecks from 'lib/api/selectDecks';
+import { ConnectorIcons } from './ConnectorIcons';
+
+type Props = {
+  onClose: () => void;
+  redirectToOnboarding?: () => void;
+};
+
+export default function ConnectModal(props: Props) {
+  const { onClose, redirectToOnboarding } = props;
+  const router = useRouter();
+  const { connectors } = useConnect();
+  const { user, signIn } = useAuth();
+
+  const hotkeys = useMemo(
+    () => [
+      {
+        hotkey: 'esc',
+        callback: () => onClose(),
+      },
+    ],
+    [onClose],
+  );
+  useHotkeys(hotkeys);
+
+  useEffect(() => {
+    const onboardUser = async (userId: string) => {
+      const decks = await selectDecks(userId, localStorage.getItem('dbToken') ?? '');
+      if (decks.length) {
+        router.push(`/app/${decks[decks.length - 1].id}`);
+      } else {
+        onClose();
+        redirectToOnboarding?.();
+      }
+    };
+
+    if (user && redirectToOnboarding) {
+      onboardUser(user.id);
+    } else if (user) {
+      onClose();
+    }
+  }, [user, router, onClose, redirectToOnboarding]);
+
+  return (
+    <div className="fixed inset-0 z-20 overflow-y-auto">
+      <div className="fixed inset-0 bg-black opacity-30" onClick={onClose} />
+      <div className="flex items-center justify-center h-screen">
+        <div className="z-30 flex flex-col w-full h-full max-w-full overflow-hidden bg-gray-900 rounded sm:max-h-128 sm:w-[605px] shadow-popover border border-gray-600 text-center pb-8">
+          <div className="flex justify-end items-center pr-2 pt-1">
+            <button className="mr-[-4px] text-gray-300 hover:text-gray-100" onClick={onClose}>
+              <IconX size={20} />
+            </button>
+          </div>
+          <h2 className="text-3xl my-12 font-semibold tracking-wider">Connect wallet</h2>
+          <div className="flex flex-col gap-6 items-center">
+            {connectors.map(connector => {
+              const ConnectorIcon = ConnectorIcons[connector.id];
+              return (
+                <button
+                  className={`flex items-center justify-between w-1/2 py-2 px-4 rounded border border-gray-700 ${
+                    !connector.ready ? 'cursor-not-allowed' : 'hover:bg-gray-800 focus:bg-gray-800'
+                  }`}
+                  disabled={!connector.ready}
+                  key={connector.id}
+                  onClick={async () => await signIn(connector)}
+                >
+                  {connector.name}
+                  <ConnectorIcon className="h-7 w-7" />
+                </button>
+              );
+            })}
+            <button
+              className="flex items-center justify-between w-1/2 py-2 px-4 rounded hover:bg-gray-800 focus:bg-gray-800 border border-gray-700"
+              onClick={() =>
+                window.open('https://ethereum.org/en/wallets/find-wallet/', '_blank', 'noopener noreferrer')
+              }
+            >
+              I don&apos;t have a wallet
+              <IconWallet size={26} className="text-gray-400" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/onboarding/ConnectorIcons.tsx
@@ -0,0 +1,252 @@
+type IconProps = {
+  className?: string;
+};
+
+export const ConnectorIcons: { [key: string]: (props: IconProps) => JSX.Element } = {
+  metaMask: (props: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" fill="none" {...props}>
+      <rect width="28" height="28" fill="none" />
+      <path
+        d="M24.0891 3.1199L15.3446 9.61456L16.9617 5.7828L24.0891 3.1199Z"
+        fill="#E2761B"
+        stroke="#E2761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.90207 3.1199L12.5763 9.67608L11.0383 5.7828L3.90207 3.1199Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.9429 18.1745L18.6139 21.7426L23.597 23.1136L25.0295 18.2536L20.9429 18.1745Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.97929 18.2536L4.40301 23.1136L9.38607 21.7426L7.05713 18.1745L2.97929 18.2536Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.10483 12.1456L7.71626 14.2461L12.6642 14.4658L12.4884 9.14877L9.10483 12.1456Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.8864 12.1456L15.4589 9.08725L15.3446 14.4658L20.2837 14.2461L18.8864 12.1456Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.38606 21.7426L12.3566 20.2925L9.79033 18.2888L9.38606 21.7426Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.6347 20.2925L18.6139 21.7426L18.2009 18.2888L15.6347 20.2925Z"
+        fill="#E4761B"
+        stroke="#E4761B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.6139 21.7426L15.6347 20.2925L15.8719 22.2348L15.8456 23.0521L18.6139 21.7426Z"
+        fill="#D7C1B3"
+        stroke="#D7C1B3"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.38606 21.7426L12.1544 23.0521L12.1368 22.2348L12.3566 20.2925L9.38606 21.7426Z"
+        fill="#D7C1B3"
+        stroke="#D7C1B3"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.1984 17.0056L9.72002 16.2762L11.4689 15.4765L12.1984 17.0056Z"
+        fill="#233447"
+        stroke="#233447"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.7928 17.0056L16.5223 15.4765L18.28 16.2762L15.7928 17.0056Z"
+        fill="#233447"
+        stroke="#233447"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.38606 21.7426L9.80791 18.1745L7.05712 18.2536L9.38606 21.7426Z"
+        fill="#CD6116"
+        stroke="#CD6116"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.1921 18.1745L18.6139 21.7426L20.9429 18.2536L18.1921 18.1745Z"
+        fill="#CD6116"
+        stroke="#CD6116"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.2837 14.2461L15.3446 14.4658L15.8016 17.0057L16.5311 15.4765L18.2888 16.2762L20.2837 14.2461Z"
+        fill="#CD6116"
+        stroke="#CD6116"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.72002 16.2762L11.4777 15.4765L12.1984 17.0057L12.6642 14.4658L7.71626 14.2461L9.72002 16.2762Z"
+        fill="#CD6116"
+        stroke="#CD6116"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.71626 14.2461L9.79033 18.2888L9.72002 16.2762L7.71626 14.2461Z"
+        fill="#E4751F"
+        stroke="#E4751F"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.2888 16.2762L18.2009 18.2888L20.2837 14.2461L18.2888 16.2762Z"
+        fill="#E4751F"
+        stroke="#E4751F"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.6642 14.4658L12.1984 17.0057L12.7784 20.0025L12.9102 16.0565L12.6642 14.4658Z"
+        fill="#E4751F"
+        stroke="#E4751F"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.3446 14.4658L15.1073 16.0477L15.2128 20.0025L15.8016 17.0057L15.3446 14.4658Z"
+        fill="#E4751F"
+        stroke="#E4751F"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.8016 17.0056L15.2128 20.0025L15.6347 20.2925L18.2009 18.2888L18.2888 16.2762L15.8016 17.0056Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.72002 16.2762L9.79033 18.2888L12.3566 20.2925L12.7784 20.0025L12.1984 17.0056L9.72002 16.2762Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.8456 23.0521L15.8719 22.2348L15.6522 22.0414H12.339L12.1368 22.2348L12.1544 23.0521L9.38606 21.7426L10.3528 22.5336L12.3126 23.8958H15.6786L17.6472 22.5336L18.6139 21.7426L15.8456 23.0521Z"
+        fill="#C0AD9E"
+        stroke="#C0AD9E"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.6347 20.2925L15.2128 20.0025H12.7784L12.3566 20.2925L12.1368 22.2348L12.339 22.0414H15.6522L15.8719 22.2348L15.6347 20.2925Z"
+        fill="#161616"
+        stroke="#161616"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24.4583 10.0364L25.2053 6.45072L24.0891 3.1199L15.6347 9.39485L18.8864 12.1456L23.4827 13.4903L24.5022 12.3038L24.0628 11.9874L24.7658 11.3459L24.221 10.924L24.924 10.3879L24.4583 10.0364Z"
+        fill="#763D16"
+        stroke="#763D16"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.79472 6.45072L3.54174 10.0364L3.06717 10.3879L3.77024 10.924L3.23415 11.3459L3.93722 11.9874L3.4978 12.3038L4.50847 13.4903L9.10483 12.1456L12.3566 9.39485L3.90207 3.1199L2.79472 6.45072Z"
+        fill="#763D16"
+        stroke="#763D16"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.4827 13.4903L18.8864 12.1456L20.2837 14.2461L18.2009 18.2888L20.9429 18.2536H25.0295L23.4827 13.4903Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.10484 12.1456L4.50848 13.4903L2.97929 18.2536H7.05713L9.79033 18.2888L7.71626 14.2461L9.10484 12.1456Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.3446 14.4658L15.6347 9.39485L16.9705 5.7828H11.0383L12.3566 9.39485L12.6642 14.4658L12.7696 16.0653L12.7784 20.0025H15.2128L15.2304 16.0653L15.3446 14.4658Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        strokeWidth="0.0878845"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  walletConnect: (props: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" fill="none" {...props}>
+      <rect width="28" height="28" fill="none" />
+      <path
+        d="M8.38969 10.3739C11.4882 7.27538 16.5118 7.27538 19.6103 10.3739L19.9832 10.7468C20.1382 10.9017 20.1382 11.1529 19.9832 11.3078L18.7076 12.5835C18.6301 12.6609 18.5045 12.6609 18.4271 12.5835L17.9139 12.0703C15.7523 9.9087 12.2477 9.9087 10.0861 12.0703L9.53655 12.6198C9.45909 12.6973 9.3335 12.6973 9.25604 12.6198L7.98039 11.3442C7.82547 11.1893 7.82547 10.9381 7.98039 10.7832L8.38969 10.3739ZM22.2485 13.012L23.3838 14.1474C23.5387 14.3023 23.5387 14.5535 23.3838 14.7084L18.2645 19.8277C18.1096 19.9827 17.8584 19.9827 17.7035 19.8277C17.7035 19.8277 17.7035 19.8277 17.7035 19.8277L14.0702 16.1944C14.0314 16.1557 13.9686 16.1557 13.9299 16.1944C13.9299 16.1944 13.9299 16.1944 13.9299 16.1944L10.2966 19.8277C10.1417 19.9827 9.89053 19.9827 9.73561 19.8278C9.7356 19.8278 9.7356 19.8277 9.7356 19.8277L4.61619 14.7083C4.46127 14.5534 4.46127 14.3022 4.61619 14.1473L5.75152 13.012C5.90645 12.857 6.15763 12.857 6.31255 13.012L9.94595 16.6454C9.98468 16.6841 10.0475 16.6841 10.0862 16.6454C10.0862 16.6454 10.0862 16.6454 10.0862 16.6454L13.7194 13.012C13.8743 12.857 14.1255 12.857 14.2805 13.012C14.2805 13.012 14.2805 13.012 14.2805 13.012L17.9139 16.6454C17.9526 16.6841 18.0154 16.6841 18.0541 16.6454L21.6874 13.012C21.8424 12.8571 22.0936 12.8571 22.2485 13.012Z"
+        fill="#3B99FC"
+      />
+    </svg>
+  ),
+};
