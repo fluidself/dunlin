72121f420669445df50127a939c18c92e01e642e
fluidself <me@fluidself.org>
2023-04-09 10:36:55 +0200

    Fixes note header identicon alignment

--- components/editor/NoteHeader.tsx
+++ components/editor/NoteHeader.tsx
@@ -227,10 +227,12 @@ export default function NoteHeader() {
                 </Tooltip>
               )}
               <NoteHeaderDivider />
-              <div className="px-2 pt-1 pb-1 text-sm text-gray-600 overflow-ellipsis dark:text-gray-400">
-                {addEllipsis(user.id)}
+              <div className="flex items-center mr-2">
+                <div className="px-2 text-sm text-gray-600 overflow-ellipsis dark:text-gray-400">
+                  {addEllipsis(user.id)}
+                </div>
+                <Identicon diameter={16} className="w-4 h-4 mb-1" />
               </div>
-              <Identicon diameter={16} className="w-5 h-5 mr-2" />
               <NoteHeaderDivider />
             </div>
           )}
