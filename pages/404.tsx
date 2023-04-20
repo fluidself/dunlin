7b2f2520ed07a4300e884b4cd1c0c8cde7b0cb7f
fluidself <me@fluidself.org>
2023-04-20 08:07:39 +0200

    Adds custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-900 text-gray-100">
+      <h1 className="text-2xl mr-8">404</h1>
+      <h2 className="leading-8">This page could not be found.</h2>
+    </div>
+  );
+}
