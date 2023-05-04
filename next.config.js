f07175a86d11c6105bc60b7310fe6c81ebab350b
fluidself <me@fluidself.org>
2023-05-04 08:14:36 +0200

    Enables support for webassembly modules

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   reactStrictMode: true,
+  webpack(config) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
   async redirects() {
     return [
       {
