e704b516dfc3e1020a97bcd96eeb257aeee086f6
fluidself <me@fluidself.org>
2023-05-10 07:57:18 +0200

    Adds daemon button to hovering toolbar

--- components/editor/toolbar/HoveringToolbar.tsx
+++ components/editor/toolbar/HoveringToolbar.tsx
@@ -11,24 +11,32 @@ import {
 import { memo } from 'react';
 import { useFocused } from 'slate-react';
 import { Mark } from 'types/slate';
+import { useStore } from 'lib/store';
 import { isMobile, modifierKey } from 'utils/device';
 import type { AddLinkPopoverState } from '../Editor';
+import type { DaemonPopoverState } from '../DaemonPopover';
 import EditorPopover from '../EditorPopover';
 import FormatButton from './FormatButton';
 import LinkButton from './LinkButton';
+import DaemonButton from './DaemonButton';
 
 type Props = {
   setAddLinkPopoverState: (state: AddLinkPopoverState) => void;
+  setDaemonPopoverState: (state: DaemonPopoverState) => void;
 };
 
 function HoveringToolbar(props: Props) {
-  const { setAddLinkPopoverState } = props;
+  const { setAddLinkPopoverState, setDaemonPopoverState } = props;
+  const isDaemonUser = useStore(state => state.isDaemonUser);
   const focused = useFocused();
   const key = modifierKey();
 
   return (
     <EditorPopover placement={isMobile() ? 'bottom-start' : 'top-start'} className={`${!focused ? 'hidden' : ''}`}>
       <LinkButton setAddLinkPopoverState={setAddLinkPopoverState} className="border-r dark:border-gray-700" />
+      {isDaemonUser ? (
+        <DaemonButton setDaemonPopoverState={setDaemonPopoverState} className="border-r dark:border-gray-700" />
+      ) : null}
       <FormatButton format={Mark.Bold} Icon={IconBold} tooltip={`Bold (${key}+B)`} aria-label="Bold" />
       <FormatButton format={Mark.Italic} Icon={IconItalic} tooltip={`Italic (${key}+I)`} aria-label="Italic" />
       <FormatButton
