9125371f02cb4c81f0f35f24ddee84a85354ca92
fluidself <me@fluidself.org>
2023-07-16 09:12:29 +0200

    Refactors daemon sidebar components using new api

--- components/daemon/DaemonMessage.tsx
+++ components/daemon/DaemonMessage.tsx
@@ -1,39 +1,39 @@
 import { memo } from 'react';
 import { IconCopy, IconGhost2 } from '@tabler/icons';
+import type { Message } from 'ai/react';
 import classNames from 'classnames';
 import remarkGfm from 'remark-gfm';
 import ReactMarkdown from 'lib/react-markdown';
-import type { DaemonMessage } from 'lib/createDaemonSlice';
 import copyToClipboard from 'utils/copyToClipboard';
 import Identicon from 'components/Identicon';
 import CodeBlock from './CodeBlock';
 
-type MessageProps = {
-  message: DaemonMessage;
+type DaemonMessageProps = {
+  message: Message;
   messageIsLatest: boolean;
   messageIsStreaming: boolean;
 };
 
-function Message(props: MessageProps) {
+function DaemonMessage(props: DaemonMessageProps) {
   const {
-    message: { type, text },
+    message: { role, content },
     messageIsLatest,
     messageIsStreaming,
   } = props;
   const messageClassName = classNames(
     'flex w-full space-x-2 py-4 pl-2 dark:text-gray-200',
-    { 'bg-gray-100 dark:bg-gray-800': type === 'human' },
-    { 'bg-gray-50 dark:bg-gray-700': type === 'ai' },
+    { 'bg-gray-100 dark:bg-gray-800': role === 'user' },
+    { 'bg-gray-50 dark:bg-gray-700': role === 'assistant' },
   );
 
   return (
     <div className={messageClassName}>
-      <div>{type === 'human' ? <Identicon diameter={20} className="w-5 h-5" /> : <IconGhost2 size={20} />}</div>
-      {type === 'human' ? (
-        <div className="whitespace-pre-line overflow-x-auto">{text}</div>
+      <div>{role === 'user' ? <Identicon diameter={20} className="w-5 h-5" /> : <IconGhost2 size={20} />}</div>
+      {role === 'user' ? (
+        <div className="whitespace-pre-line overflow-x-auto">{content}</div>
       ) : (
         <div className="relative flex flex-col w-[calc(100%-50px)] lg:w-[calc(100%-60px)] h-full">
-          {text ? (
+          {content ? (
             <ReactMarkdown
               className="prose dark:prose-invert max-w-none overflow-x-auto prose-p:whitespace-pre-line prose-a:text-primary-400 hover:prose-a:underline prose-pre:m-0 prose-pre:p-0 prose-pre:bg-gray-100 prose-pre:dark:bg-gray-800 prose-pre:text-gray-800 prose-pre:dark:text-gray-100 prose-code:bg-gray-100 prose-code:dark:bg-gray-800 prose-code:text-gray-800 prose-code:dark:text-gray-100"
               linkTarget="_blank"
@@ -82,23 +82,23 @@ function Message(props: MessageProps) {
                 },
               }}
             >
-              {`${text}${messageIsLatest && messageIsStreaming ? '`▍`' : ''}`}
+              {`${content}${messageIsLatest && messageIsStreaming ? '`▍`' : ''}`}
             </ReactMarkdown>
           ) : (
             <span className="animate-pulse">▍</span>
           )}
         </div>
       )}
-      {type === 'ai' ? (
+      {role === 'assistant' ? (
         <IconCopy
           size={18}
           className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
           role="button"
-          onClick={async () => await copyToClipboard(text)}
+          onClick={async () => await copyToClipboard(content)}
         />
       ) : null}
     </div>
   );
 }
 
-export default memo(Message);
+export default memo(DaemonMessage);
