3fede1576b82fb7033f1635964a24da0defb4847
fluidself <me@fluidself.org>
2024-05-04 10:48:22 +0200

    Updates daemon model pointer

--- lib/store.ts
+++ lib/store.ts
@@ -45,7 +45,7 @@ export enum SidebarTab {
 }
 
 export enum DaemonModel {
-  'gpt-3.5-turbo' = 'gpt-3.5-turbo-1106',
+  'gpt-3.5-turbo' = 'gpt-3.5-turbo',
   'gpt-4' = 'gpt-4',
   'gpt-4-turbo' = 'gpt-4-turbo',
 }
