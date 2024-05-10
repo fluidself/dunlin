7f843d0b9df88c07aa37d4894c4eaaaa3376ee47
fluidself <me@fluidself.org>
2024-05-10 10:47:58 +0200

    Handles multiple sessions in daemon store

--- lib/createDaemonSlice.ts
+++ lib/createDaemonSlice.ts
@@ -8,17 +8,29 @@ type DaemonMessage = {
   content: string;
 };
 
+export type DaemonSession = {
+  id: string;
+  title: string;
+  createdAt: string;
+  messages: DaemonMessage[];
+};
+
 export type DaemonStore = {
+  daemonSessions: Record<string, DaemonSession>;
   isDaemonUser: boolean;
   setIsDaemonUser: Setter<boolean>;
   isDaemonSidebarOpen: boolean;
   setIsDaemonSidebarOpen: Setter<boolean>;
-  messages: DaemonMessage[];
-  setMessages: Setter<DaemonMessage[]>;
   model: DaemonModel;
   setModel: Setter<DaemonModel>;
   temperature: number;
   setTemperature: Setter<number>;
+  activeDaemonSession: string;
+  setActiveDaemonSession: Setter<string>;
+  insertDaemonSession: (session: DaemonSession) => void;
+  renameDaemonSession: (sessionId: string, title: string) => void;
+  deleteDaemonSession: (sessionId: string) => void;
+  addMessageToDaemonSession: (sessionId: string, message: DaemonMessage) => void;
 };
 
 const createDaemonSlice = (set: (fn: (draft: Draft<Store>) => void) => void) => ({
@@ -26,12 +38,34 @@ const createDaemonSlice = (set: (fn: (draft: Draft<Store>) => void) => void) => 
   setIsDaemonUser: setter(set, 'isDaemonUser'),
   isDaemonSidebarOpen: false,
   setIsDaemonSidebarOpen: setter(set, 'isDaemonSidebarOpen'),
-  messages: [],
-  setMessages: setter(set, 'messages'),
   model: DaemonModel['gpt-3.5-turbo'],
   setModel: setter(set, 'model'),
   temperature: 0,
   setTemperature: setter(set, 'temperature'),
+  activeDaemonSession: '',
+  setActiveDaemonSession: setter(set, 'activeDaemonSession'),
+  daemonSessions: {},
+  insertDaemonSession: (session: DaemonSession) =>
+    set(state => {
+      state.daemonSessions[session.id] = session;
+    }),
+  deleteDaemonSession: (sessionId: string) =>
+    set(state => {
+      delete state.daemonSessions[sessionId];
+    }),
+  renameDaemonSession: (sessionId: string, title: string) =>
+    set(state => {
+      if (state.daemonSessions[sessionId]) {
+        state.daemonSessions[sessionId].title = title;
+      }
+    }),
+  addMessageToDaemonSession: (sessionId: string, message: DaemonMessage) =>
+    set(state => {
+      if (state.daemonSessions[sessionId]) {
+        const messages = state.daemonSessions[sessionId].messages;
+        state.daemonSessions[sessionId].messages = [...messages, message];
+      }
+    }),
 });
 
 export default createDaemonSlice;
